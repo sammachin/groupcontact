422eb29655a39c61904564d5172e4fddf6ca005e
Sam Machin <sam@sammachin.com>
2012-09-07 11:33:39 +0100

    Lots of new stuff
    Now add incoming recordings to a google doc

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 import os
 import webapp2
 from twilio.rest import TwilioRestClient
+from twilio import twiml
 import creds
 from google.appengine.api import users
 from google.appengine.ext import webapp
@@ -30,60 +31,42 @@ import sys
 import string
 import unicodedata, re
 import urllib
-import random
 import gspread
 
 
 
-def makecode(group):
-	seed = str(random.randrange(100000, 999999, 1))
-	seed += group
-	seed += str(random.randrange(1000, 9999, 1))
-	check = int(seed) % 9
-	seed += str(check)
-	return seed
-
-
-def getgroup(code):
-	num , check = code[:-1], code[-1]
-	if int(num) % 9 == int(check):
-		pad = num[:7]
-		group = pad[-1]
-		return int(group)
-	else:
-		return False
-
-
-def sendemail(member):
-	message = """
-		Dear Consumer:
-
-			You have been allocated to the {team} faction,
-			You need to tell us your mobile number via the following link:
-			http://opshout.appspot.com/join?invitecode={invitecode}
-
-			The Authority
-		""".format(team=member['team'], invitecode=member['invitecode'])
-	mail.send_mail(sender="The Authority <sam.machin@gmail.com>",
-	              to=member['email'],
-	              subject="Join a faction",
-	              body=message)
-
-groups  = ['red', 'blue', 'green', 'orange']
-
-class Members(db.Model):
-	msisdn = db.StringProperty()
-	name = db.StringProperty()
-	group = db.StringProperty()
-	created = db.DateTimeProperty(auto_now_add=True)
-
-class Recordings(db.Model):
-	number = db.StringProperty()
-	url = db.StringProperty()
-	created = db.DateTimeProperty(auto_now_add=True)
+def getmembers(group, sheet):
+	gc = gspread.login(creds.googleuser, creds.googlepass)
+	spreadsheet = gc.open(sheet)
+	worksheet = spreadsheet.sheet1
+	members = []
+	for x in range(1, len(worksheet.col_values(1))):
+		if worksheet.col_values(2)[x] == group:
+			members.append(worksheet.col_values(1)[x])
+	return members
 	
 
+def getallmembers(sheet):
+	gc = gspread.login(creds.googleuser, creds.googlepass)
+	spreadsheet = gc.open(sheet)
+	worksheet = spreadsheet.sheet1
+	members = []
+	for x in range(1, len(worksheet.col_values(1))):
+		members.append(worksheet.col_values(1)[x])
+	return members
+		
+		
+def geturl(msgid, sheet):
+	gc = gspread.login(creds.googleuser, creds.googlepass)
+	spreadsheet = gc.open(sheet)
+	worksheet = spreadsheet.sheet1
+	url = ""
+	for x in range(1, len(worksheet.col_values(1))):
+		if worksheet.col_values(1)[x] == msgid:
+			url = worksheet.col_values(2)[x]
+	return url
 
+		
 class sendsms(webapp2.RequestHandler):
 	def get(self):
 		if users.get_current_user():
@@ -96,95 +79,108 @@ class sendsms(webapp2.RequestHandler):
 		if users.get_current_user():
 			group = self.request.get('group')	
 			message = self.request.get('message')
-			query = Members.all()
-			query.filter("group =", group) 
-			results = query.fetch(1000)
+			members = getmembers(group, "groups")
 			client = TwilioRestClient(creds.twilioaccount, creds.twiliotoken)
-			for member in results:
-				client.sms.messages.create(from_="+441172001500", to=member.msisdn, body=message)
+			for member in members:
+				client.sms.messages.create(from_="+441172001500", to=member, body=message)
+			self.response.out.write("SMS sent to %s members of %s" % (str(len(members)), group))
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 
 
-
-class join(webapp2.RequestHandler):
-	def get(self):
-		invitecode = self.request.get('invitecode')
-		teamnum = getgroup(invitecode)
-		if teamnum:
-			template_values = {'invitecode' : invitecode,  'team' : groups[teamnum] }
-			path = os.path.join(os.path.dirname(__file__), 'joininvite.html')
-			self.response.out.write(template.render(path, template_values))
-		else:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('Invalid Invite Code')
-	def post(self):
-		msisdn = self.request.get('msisdn')
-		invitecode = self.request.get('invitecode')
-		teamnum = getgroup(invitecode)
-		if teamnum:
-			group = groups[teamnum]
-			member = Members()
-			member.msisdn = msisdn
-			member.group = group
-			member.put()
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('OK')
-		else:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('Invalid Invite Code')
-			
-
-class joingroup(webapp2.RequestHandler):
-	def get(self):
-		template_values = {}
-		path = os.path.join(os.path.dirname(__file__), 'joingroup.html')
-		self.response.out.write(template.render(path, template_values))
-	def post(self):
-		msisdn = self.request.get('msisdn')
-		group = self.request.get('group')
-		member = Members()
-		member.msisdn = msisdn
-		member.group = group
-		member.put()
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.out.write('OK')
-
-
-class sendinvites(webapp2.RequestHandler):
+class smsall(webapp2.RequestHandler):
 	def get(self):
 		if users.get_current_user():
 			template_values = {}
-			path = os.path.join(os.path.dirname(__file__), 'sendinvites.html')
+			path = os.path.join(os.path.dirname(__file__), 'sendsms.html')
 			self.response.out.write(template.render(path, template_values))
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
 	def post(self):
-		sheetname = self.request.get('sheet')
+		if users.get_current_user():
+			group = self.request.get('group')	
+			message = self.request.get('message')
+			members = getallmembers("groups")
+			client = TwilioRestClient(creds.twilioaccount, creds.twiliotoken)
+			for member in members:
+				client.sms.messages.create(from_="+441172001500", to=member, body=message)
+				self.response.out.write("SMS sent to %s members " % (str(len(members))))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+						
+						
+class groupcall(webapp2.RequestHandler):
+	def get(self):
+		if users.get_current_user():
+			template_values = {}
+			path = os.path.join(os.path.dirname(__file__), 'groupcall.html')
+			self.response.out.write(template.render(path, template_values))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+	def post(self):
+		if users.get_current_user():
+			group = self.request.get('group')	
+			message = self.request.get('message')
+			members = getmembers(group, "groups")
+			client = TwilioRestClient(creds.twilioaccount, creds.twiliotoken)
+			for member in members:
+				client.calls.create(from_="+441172001500", to=member, url="http://opshout.appspot.com/callone?msgid=%s" % (message))
+			self.response.out.write("Calls placed to %s members of %s" % (str(len(members)), group))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
+class callone(webapp2.RequestHandler):
+	def post(self):
+		sheet = "recordings"
+		msgid = self.request.get('msgid')
+		url = geturl(msgid, sheet)
+		r = twiml.Response()
+		r.play(url)
+		self.response.out.write(str(r))
+
+class incommingcall(webapp2.RequestHandler):
+	def post(self):
+		r = twiml.Response()
+		r.say("We will contact you when we are ready, do not call this number again")
+		self.response.out.write(str(r))			
+
+
+class recordcall(webapp2.RequestHandler):
+	def get(self):
+		r = twiml.Response()
+		r.say("hello please record your message after the tone")
+		r.record(method="POST", action="http://opshout.appspot.com/storerecording", maxLength="60", timeout="5")
+		self.response.out.write(str(r))
+				
+				
+class storerecording(webapp2.RequestHandler):
+	def post(self):
+		caller = self.request.get('From')	
+		url = self.request.get('RecordingUrl')	
 		gc = gspread.login(creds.googleuser, creds.googlepass)
-		spreadsheet = gc.open(sheetname)
+		spreadsheet = gc.open("newrecordings")
 		worksheet = spreadsheet.sheet1
-		members = []
-		for x in range(1, len(worksheet.col_values(1))):
-			teamnum = worksheet.col_values(2)[x]
-			member = {}
-			member['email'] = worksheet.col_values(1)[x]
-			member['invitecode'] = makecode(teamnum)
-			member['team'] = groups[int(teamnum)]
-			members.append(member)
-		for member in members:
-			sendemail(member)
-		self.response.out.write('OK')
-			
-	
+		count = len(worksheet.col_values(1))
+		worksheet.update_cell(count+1, 1, caller)
+		worksheet.update_cell(count+1, 2, url)
+		r = twiml.Response()
+		r.say("Thankyou, goodbye")
+		self.response.out.write(str(r))
+						
+						
+						
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('This is The Authority')
+        self.response.out.write('This is OpShout')
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 								('/sendsms', sendsms),
-								('/joingroup', joingroup),
-								('/join', join),
-								('/sendinvites', sendinvites),
+								('/smsmall', smsall),
+								('/groupcall', groupcall),
+								('/callone', callone),
+								('/incommingcall', incommingcall),
+								('/recordcall', recordcall),
+								('/storerecording', storerecording)
 								],
                               debug=True)
